8f6e667a2361c2e06baf7cfc5bb11ab88e5cfdd2
John Fletcher <john@bunbury28.plus.com>
2021-07-13 16:13:21 +0100

    Update TCRT5000_LED.ino
    Some changes including explanation of pin settings to get digital signal.

--- TCRT5000_LED/TCRT5000_LED.ino
+++ TCRT5000_LED/TCRT5000_LED.ino
@@ -2,11 +2,13 @@
 // I have now had this working.
 // I now want to add an LED to indicate that a detection has been made.
 // (c) John Fletcher (M6777)
-//
+//                                                          
 // The IR signal is pulled high and goes low when there is a detection.
+// This gives a digital signal instead of an analog one.
+// I have not seen this documented. It is what Terry Bailey uses.
 #define SIGNAL_PIN 5
 #define IR_PIN 6
-#define LED_PIN 7
+#define LED_PIN 13
 
 int ir_signal = 0;
 int previous_signal = -1;
@@ -26,11 +28,11 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   ir_signal = digitalRead(SIGNAL_PIN);
-  Serial.println(ir_signal);
   delayMicroseconds(500);
   // Only do this if signal has changed.
   if (ir_signal != previous_signal) {
     previous_signal = ir_signal;
+    Serial.println(ir_signal);
     if (ir_signal == 0) {
       digitalWrite(LED_PIN, HIGH);
     } else {
