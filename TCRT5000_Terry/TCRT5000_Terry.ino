473d141168ecef892d7750268ac56423da80f824
John Fletcher <john@bunbury28.plus.com>
2023-02-26 22:28:19 +0000

    Update TCRT5000_Terry.ino
    Add heading code

--- TCRT5000_Terry/TCRT5000_Terry.ino
+++ TCRT5000_Terry/TCRT5000_Terry.ino
@@ -2,6 +2,20 @@
 // I have now had this working.
 // John Fletcher (M6777)
 //
+// 3rd party libraries
+#include <Streaming.h>
+
+const byte VER_MAJ  = 1;
+const byte VER_MIN  = 0;
+const byte VER_DETAIL = 0;
+
+void heading()
+{
+  Serial << endl << endl << __FILE__ << endl;
+  Serial << F("Ver: ") << VER_MAJ << F(".") << VER_MIN << F(".") << VER_DETAIL;
+  Serial << F(" compiled on ") << __DATE__ << F(" at ") << __TIME__ << F(" using compiler ") << __cplusplus << endl;
+}
+
 // The signal is pulled high and goes low when there is a signal.
 #define SIGNAL_PIN 5
 #define LED_PIN 6
@@ -9,7 +23,9 @@
 int ir_signal = 0;
 
 void setup() {
-  Serial.begin(9600);
+  while(!Serial);
+  Serial.begin(115200);
+  heading();
   // put your setup code here, to run once:
   pinMode(SIGNAL_PIN,INPUT_PULLUP);
   pinMode(LED_PIN,OUTPUT);
